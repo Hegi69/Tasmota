5993938534ac24758ae22acbbd5768de9cf09ca9
Hegi69 <155374776+Hegi69@users.noreply.github.com>
2024-02-10 12:13:27 +0100

    Update user_config_override_sample.h

--- tasmota/user_config_override_sample.h
+++ tasmota/user_config_override_sample.h
@@ -87,7 +87,15 @@ Examples :
 // !!! Remember that your changes GOES AT THE BOTTOM OF THIS FILE right before the last #endif !!!
 */
 
-
+#ifndef USE_SCRIPT
+#define USE_SCRIPT
+#endif
+#ifndef USE_SML_M
+#define USE_SML_M
+#endif
+#ifdef USE_RULES
+#undef USE_RULES
+#endif
 
 
 
